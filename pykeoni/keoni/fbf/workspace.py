3ed28b5b0257dff5848ec2a6bc6d8671edc3d41b
rayg <rayg@b7ca1e74-3157-0410-8c33-a17d4927038d>
2011-09-11 17:22:28 +0000

    added .variables() and .var call similar to pycdf also fixed ambiguity problem with using glob* - changed to glob.* so that W.Wavenumber works
    git-svn-id: https://svn.ssec.wisc.edu/repos/rayg/trunk@499 b7ca1e74-3157-0410-8c33-a17d4927038d

--- pykeoni/keoni/fbf/workspace.py
+++ pykeoni/keoni/fbf/workspace.py
@@ -4,7 +4,7 @@ Flat Binary Format python utilities
 parallel to TOOLS/Mfiles/fbf*.m
 """
 
-import glob,exceptions
+import os,glob,exceptions
 
 from keoni.fbf.numfbf import *  # originally in cvs/TOOLS/dev/maciek/python
 
@@ -13,7 +13,7 @@ class Workspace(object):
         self._dir=dir
 
     def __getattr__(self,name):
-        g = glob.glob( os.path.join(self._dir,name+'*') )
+        g = glob.glob( os.path.join(self._dir,name+'.*') )
         if len(g)==1:
             fp = FBF(g[0])
             fp.open()
@@ -21,6 +21,20 @@ class Workspace(object):
             return fp
         raise exceptions.AttributeError, "%s not in workspace" % name
         
+    def var(self,name):
+        return getattr(self, name)
+    
+    def vars(self):
+        for path in os.listdir(self._dir):
+            try:
+                x = info(path)
+                yield x.stemname, x
+            except:
+                pass
+            
+    def variables(self):
+        return dict(self.vars())
+
     def __getitem__(self,name):
         return getattr(self,name)
 
