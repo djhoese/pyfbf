a348b0c6c708f8945e583d691e0c7e8841627716
rayg <rayg@6d26d614-2410-46aa-98d6-946711d5b2fa>
2012-08-12 21:08:07 +0000

    test pattern OK for rolling and non-rolling externally triggered fbf2dpl
    git-svn-id: http://svn.ssec.wisc.edu/repos/ifg_shis/trunk@3615 6d26d614-2410-46aa-98d6-946711d5b2fa

--- pyfbf/fbf.py
+++ pyfbf/fbf.py
@@ -13,38 +13,142 @@ from .slicer import fbf_slicer
 
 LOG = logging.getLogger(__name__)
 
+def _onesies():
+    n = 0
+    while True:
+        n+=1
+        yield n
+
 
 class fbf2dpl(object):
     """convert a FBF non-circular working directory into a DPL frame stream
     """
-    _slicer = None            # fbf_slicer instance 
+    _work_dir = None          # data we're observing
+    _slicer = None            # fbf_slicer instance
     _eob_sleep = None         # minimum time delay between frames in seconds
-    
-    def __init__(self, work_dir, eob_sleep=None, filename_filter=None, realtime=None):
+    _rec_gen = None           # source for record numbers, _onesies() if nothing else provided
+    _frame_width = None       # number of records in a frame
+    _rolling = None           # whether frames should overlap as new records come in
+
+    def __init__(self, work_dir, frame_width=1, buffer_size=0, filename_filter=None, rolling=False, eob_sleep=None, rec_gen=None):
         """
         start up fpf2dpl for a given rsh2fbf work directory
         optionally provide a filter predicate for which filenames to open into the frame
-        if rsh2fbf.pid is present and realtime is True or None (automatic), hook into UDP signals
+        optionally provide a record generator which blocks on data availability events, e.g. ifg.core.udp_events
+            if record_generator is True and rsh2fbf.pid is available in work directory, will follow that
         """
         assert(os.path.isdir(work_dir))
+        self._work_dir = work_dir
         self._eob_sleep = eob_sleep
-        self._slicer = fbf_slicer(work_dir, 0, filename_filter)
-        
+        self._slicer = fbf_slicer(work_dir, buffer_size, filename_filter)
+        self._frame_width = frame_width
+        self._rec_gen = rec_gen
+        self._rolling = rolling
+
     def close(self):
         return
 
+    def _rsh2fbf_event_gen(self):
+        "connect to the UDP broadcasts of a running rsh2fbf using its pidfile"
+        from ifg.core.udp_events import udp_event_gen
+        LOG.debug('looking for rsh2fbf.pid')
+        fnpid = os.path.join(self._work_dir, 'rsh2fbf.pid')
+        if not os.path.isfile(fnpid):
+            LOG.warning('no rsh2fbf.pid found')
+            return None
+        with file(fnpid,'rt') as fp:
+            pid = int(fp.read().strip())
+            return udp_event_gen(yield_recs=True, MSG='FBFnewRecords', PID=pid)
+
+
     def __call__(self):
-        rec = 1
         next_time = time.time() if self._eob_sleep else None
-        while True:
+
+        if self._rec_gen is True:
+            self._rec_gen = self._rsh2fbf_event_gen()
+        if self._rec_gen is None:
+            self._rec_gen = _onesies()
+
+        for rec in self._rec_gen:
+            LOG.debug('processing record %d' % rec)
             if self._eob_sleep:
                 now = time.time()
                 delta = next_time - now
                 if delta>0.0:
                     time.sleep(delta)
                 next_time = now + self._eob_sleep
-            data = self._slicer(rec)
+
+            if not self._rolling and 0 != (rec % self._frame_width):
+                LOG.debug('skipping %d since we are not rolling' % rec)
+                continue
+
+            if rec < self._frame_width:
+                LOG.debug('waiting for %d records' % self._frame_width)
+                continue
+
+            first = max(1,rec-self._frame_width+1)
+            last = rec
+
+            LOG.debug('grabbing %d thru %d for %d' % (first,last,self._frame_width))
+            data = self._slicer(first, last)
             if data is None:
+                LOG.warning('???')
                 return
+            data.first_record, data.last_record = first, last
+            LOG.debug('done slicing')
             yield data
-            rec += 1
+
+
+def main():
+    import argparse
+    desc = """python -m ifg.rsh2dpl.fbf /tmp/rsh2fbf
+"""
+    # see http://docs.python.org/library/argparse.html
+    parser = argparse.ArgumentParser(description = desc)
+    parser.add_argument('-t', '--test',
+                    action="store_true", default=False, help="run self-tests")
+    parser.add_argument('-B', '--buffer-size', metavar='buffer_size', default=0, type=int, help='rsh2fbf circular buffer size' )
+    parser.add_argument('-R', '--rolling', default=False, action='store_true', help='rolling records' )
+    parser.add_argument('-v', '--verbosity', action="count", default=0,
+                    help='each occurrence increases verbosity 1 level through ERROR-WARNING-INFO-DEBUG')
+    parser.add_argument('src', help = "source file to process")
+    # parser.add_argument('-o', '--output',
+    #                 help='location to store output')
+    # parser.add_argument('-I', '--include',
+    #                 action="append", help="include path to append to GCCXML call")
+    args = parser.parse_args()
+
+
+
+    if args.test:
+        # FIXME - run any self-tests
+        # import doctest
+        # doctest.testmod()
+        sys.exit(2)
+
+    levels = [logging.ERROR, logging.WARN, logging.INFO, logging.DEBUG]
+    logging.basicConfig(level = min(3,levels[args.verbosity]))
+    LOG.debug('verbosity level is %d' % args.verbosity)
+
+    if not args:
+        parser.print_help()
+        return
+    test_case = """
+export STA_BCAST=10.0.1.255:5145
+mkdir /tmp/rsh2fbf; cd /tmp/rsh2fbf; RSH2FBF_CIRCULAR_BUFFER_SIZE=256 RSH2FBF_EOB_SLEEP=400 rsh2fbf ~/Data/SHIS/sh070714/dvrvcxv.rrsh ; sleep 15; rm -fr /tmp/rsh2fbf/*
+python -m ifg.rsh2dpl.fbf /tmp/rsh2fbf -B 256
+"""
+
+    fnf = lambda fn: fn.startswith('HBB') or fn.startswith('ABB')
+    f2d = fbf2dpl(args.src, frame_width=4, buffer_size=args.buffer_size, filename_filter = fnf, rolling=args.rolling, rec_gen=True)
+    for F in f2d():
+        print '\n=== record %d~%d' % (F.first_record, F.last_record)
+        print F.ABBapexTemp
+        sys.stdout.flush()
+
+
+    return 0
+
+
+if __name__=='__main__':
+    sys.exit(main())
--- pyfbf/slicer.py
+++ pyfbf/slicer.py
@@ -13,8 +13,8 @@ import keoni.fbf as fbf
 LOG = logging.getLogger(__name__)
 
 
-class rshdata(object):  
-    "read all the values from current workspace directory into attributes"    
+class rshdata(object):
+    "read all the values from current workspace directory into attributes"
     pass
 
 
@@ -24,8 +24,8 @@ class fbf_slicer(object):
     _wd = None
     _buffer_size = 0   # number of records to expect in circular buffer files
     _open_files = None
-    should_include = None # callable, returns True if a given filename should be included in the frame 
-    
+    should_include = None # callable, returns True if a given filename should be included in the frame
+
     def __init__(self, work_dir, buffer_size, filename_filter=None):
         self._wd = work_dir
         self._buffer_size = buffer_size
@@ -33,7 +33,7 @@ class fbf_slicer(object):
         if filename_filter is None:
             filename_filter = lambda filename: True
         self.should_include = filename_filter
-    
+
     def _update_open_files(self):
         for fn in glob( os.path.join(self._wd, '*') ):
             if fn not in self._open_files and self.should_include(os.path.split(fn)[-1]):
@@ -41,43 +41,44 @@ class fbf_slicer(object):
                 try:
                     nfo = fbf.FBF(fn)
                 except Exception as oops:
-                    nfo = None    
+                    nfo = None
                     LOG.info('%s could not be opened as FBF' % fn)
                     LOG.debug(repr(oops))
                 LOG.debug('found new file %s' % fn)
                 self._open_files[fn] = nfo
 
-    def __call__(self, start_record, end_record=None):
-        "retrieve a slice of a FBF directory using 1-based record range"
-        end_record = start_record + 1 if end_record is None else end_record
+    def __call__(self, first_record, last_record=None):
+        "retrieve a slice of a FBF directory using inclusive 1-based record number range, noting that last-first+1 records are returned"
+        last_record = first_record if last_record is None else last_record
         if not self._open_files:
             self._update_open_files()
         data = rshdata()
         for name, nfo in self._open_files.items():
-            if nfo is not None: 
+            if nfo is not None:
                 # note we use % in order to deal with
                 # wavenumber files that are only ever 1 record long
-                # circular buffers which are fixed length files 
+                # circular buffers which are fixed length files
                 len = nfo.length()
                 # check for non-circular buffer case and going off the end of the file
                 # note use of > since record numbers are 1-based
-                if self._buffer_size ==0 and (start_record > len or end_record > len):
+                if (self._buffer_size ==0) and (first_record > len or last_record > len):
+                    LOG.warning('length is %d but start-end is %d-%d' % (len,first_record,last_record))
                     return None
                 # check for circular buffers that aren't preallocated properly
-                if self._buffer_size > 0 and len not in (1, self._buffer_size):                    
+                if self._buffer_size > 0 and len not in (1, self._buffer_size):
                     LOG.info('buffer file %s size mismatch (%d != %d)! ignoring' % (name,len,self._buffer_size))
                 else:
                     # 0-based circular buffer
-                    start_index = (start_record - 1) % len
-                    end_index = (end_record - 1) % len
-                    if end_index >= start_index:
+                    first_index = (first_record - 1) % len
+                    last_index = (last_record - 1) % len
+                    if last_index >= first_index:
                         # Records are in one continuous line
-                        idx = slice(start_index, end_index + 1) # +1 to include last item
+                        idx = slice(first_index, last_index + 1) # +1 to include last item
                         setattr(data, nfo.stemname, nfo[idx])
                     else:
                         # Records are on two ends of the circular buffer
-                        idx1 = slice(start_index, self._buffer_size)
-                        idx2 = slice(0, end_index + 1) # +1 to include last item
+                        idx1 = slice(first_index, self._buffer_size)
+                        idx2 = slice(0, last_index + 1) # +1 to include last item
                         arr1 = nfo[idx1]
                         arr2 = nfo[idx2]
                         setattr(data, nfo.stemname, append(arr1, arr2, axis=0))
