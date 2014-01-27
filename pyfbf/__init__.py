9e2ab0ddceed1c0d5f98adc6b51fe5c48bb083b7
Ray Garcia <r.keoni@gmail.com>
2014-01-27 17:21:48 -0600

    migrated with history from svn/rayg/pykeoni/keoni/fbf

---
+++ pyfbf/__init__.py
@@ -0,0 +1,4 @@
+# __all__ = ['workspace', 'numfbf']
+
+from keoni.fbf.workspace import Workspace 
+from keoni.fbf.numfbf import *
--- pykeoni/keoni/fbf/__init__.py
+++
@@ -1,4 +0,0 @@
-# __all__ = ['workspace', 'numfbf']
-
-from keoni.fbf.workspace import Workspace 
-from keoni.fbf.numfbf import *
